3bde91d914bef35f5cbe276b3e16d9d472d8ed66
Dang An <ndangan094@gmail.com>
2023-02-08 16:11:44 +0700

    push

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter> 
     <Routes>
       <Route path="/" element={<Home />}/>
-      <Route path="/airdrop" element={<Voting />}/>
+      <Route path="/voting" element={<Voting />}/>
     </Routes>
   </BrowserRouter>
   );
